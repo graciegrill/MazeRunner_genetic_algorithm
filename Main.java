0fc474963c635b43435db4a81c44baed1d4e48c4
Grace Frizzell <g.e.frizzell@email.msmary.edu>
2024-05-08 14:54:51 -0400

    3pm update

--- Main.java
+++ Main.java
@@ -2,23 +2,14 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args){
-        Maze m  = new Maze(new int[]{8, 0}, 
+        Maze m  = new Maze(new int[]{0, 0}, 
         new int[][]{
-            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-            {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-            {0, 1, 0, 1, 0, 1, 0, 1, 1, 0},
-            {0, 1, 1, 0, 0, 0, 1, 1, 1, 0},
-            {0, 1, 1, 1, 1, 0, 0, 1, 1, 0},
-            {0, 1, 1, 1, 1, 0, 1, 1, 0, 0},
-            {0, 0, 1, 0, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 0, 1, 1, 1, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}
-        }, new int[]{1,9});
+            {1,1}
+
+        }, new int[]{0,0});
         Solver s = new Solver();
         ArrayList<moveNode> pop = s.generatePopulation(m);
-        System.out.print(pop.get(5).getPreMoves().toString());
-        System.out.println(s.geneticAlgorithm(pop, m).getPreMoves().toString());
+        System.out.println(s.geneticAlgorithm(pop, m, 300000).getPreMoves().toString());
     }
     
 }
--- Solver.java
+++ Solver.java
@@ -10,58 +10,71 @@ public class Solver {
         moves.add(loc);
         int total = 0;
         for(int i:currentMove.getPreMoves()){
-            if(i==0 && currentMove.getLoc()[1] != 0){
+            if(i==0 && currentMove.getLoc()[1] != 0){//good
                 int x = currentMove.getLoc()[0];
                 int y = currentMove.getLoc()[1] - 1;
+                System.out.println("*****");
+                System.out.print(i+"\n");
+                System.out.print(x+"\n");
+                System.out.print(y+"\n");
                 int[] loc1 = {x, y};
                 currentMove.setLoc(loc1);
-                if (m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 1){
+                if (m1.getGrid()[currentMove.getLoc()[1]][currentMove.getLoc()[0]] == 1){
                     total++;
                     continue;
                 }
-                if(currentMove.getLoc() == m1.getGoal()){
+                else if(currentMove.getLoc() == m1.getGoal()){
                     total = Integer.MAX_VALUE;
                     break;
                 }
-                if(moves.contains(currentMove.getLoc()) || m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 0){
+                else if(moves.contains(currentMove.getLoc()) || m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 0){
                     total = Integer.MIN_VALUE;
                     break;
                 }
             }
-            else if(i==1 && currentMove.getLoc()[0] != m1.getXDim() - 1){
+            else if(i==1 && currentMove.getLoc()[0] != m1.getYDim() - 1){
                 int x = currentMove.getLoc()[0]+1;
                 int y = currentMove.getLoc()[1];
+                System.out.println("*****");
                 int[] loc1 = {x, y};
+                System.out.print(i+"\n");
+                System.out.print(x+"\n");
+                System.out.print(y+"\n");
                 moves.add(loc1);
                 currentMove.setLoc(loc1);
-                if (m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 1){
+                if (m1.getGrid()[currentMove.getLoc()[1]][currentMove.getLoc()[0]] == 1){
                     total++;
                     continue;
                 }
-                if(currentMove.getLoc() == m1.getGoal()){
+                else if(currentMove.getLoc() == m1.getGoal()){
                     total = Integer.MAX_VALUE;
                     break;
                 }
-                if(moves.contains(currentMove.getLoc()) || m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 0){
+                else if(moves.contains(currentMove.getLoc()) || m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 0){
                     total = Integer.MIN_VALUE;
                     break;
                 }
             }
-            else if(i==2 && currentMove.getLoc()[1] != m1.getYDim()-1){
+            else if(i==2 && currentMove.getLoc()[1] != m1.getXDim()-1){
                 int x = currentMove.getLoc()[0];
                 int y = currentMove.getLoc()[1]+1;
                 int[] loc1 = {x, y};
+                System.out.println("*****");
+
+                System.out.print(i+"\n");
+                System.out.print(x+"\n");
+                System.out.print(y+"\n");
                 moves.add(loc1);
                 currentMove.setLoc(loc1);
-                if (m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 1){
+                if (m1.getGrid()[currentMove.getLoc()[1]][currentMove.getLoc()[0]] == 1){
                     total++;
                     continue;
                 }
-                if(currentMove.getLoc() == m1.getGoal()){
+                else if(currentMove.getLoc() == m1.getGoal()){
                     total = Integer.MAX_VALUE;
                     break;
                 }
-                if(moves.contains(currentMove.getLoc()) || m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 0){
+                else if(moves.contains(currentMove.getLoc()) || m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 0){
                     total = Integer.MIN_VALUE;
                     break;
                 }
@@ -69,18 +82,23 @@ public class Solver {
             else if(i == 3 && currentMove.getLoc()[0] != 0){
                 int x = currentMove.getLoc()[0]-1;
                 int y = currentMove.getLoc()[1];
+                System.out.println("*****");
+
+                System.out.print(i+"\n");
+                System.out.print(x+"\n");
+                System.out.print(y+"\n");
                 int[] loc1 = {x, y};
                 moves.add(loc1);
                 currentMove.setLoc(loc1);
-                if (m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 1){
+                if (m1.getGrid()[currentMove.getLoc()[1]][currentMove.getLoc()[0]] == 1){
                     total++;
                     continue;
                 }
-                if(currentMove.getLoc() == m1.getGoal()){
+                else if(currentMove.getLoc() == m1.getGoal()){
                     total = Integer.MAX_VALUE;
                     break;
                 }
-                if(moves.contains(currentMove.getLoc()) || m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 0){
+                else if(moves.contains(currentMove.getLoc()) || m1.getGrid()[currentMove.getLoc()[0]][currentMove.getLoc()[1]] == 0){
                     total = Integer.MIN_VALUE;
                     break;
                 }
@@ -99,7 +117,7 @@ public class Solver {
         int rand = r.nextInt(100);
         int loc = r.nextInt(path.size());
         int choice = r.nextInt(4);
-        if (rand<10){
+        if (rand<99){
             path.set(loc, choice);
         }
         return path;
@@ -131,36 +149,41 @@ public class Solver {
         return paths;
     }
 
-    public moveNode geneticAlgorithm(ArrayList<moveNode> pop, Maze m1){
+    public moveNode geneticAlgorithm(ArrayList<moveNode> pop, Maze m1, long maxTimeMillis){
+        long startTime = System.currentTimeMillis(); // Start timing
         int fit = 0;
         moveNode winner = new moveNode();
-        while(fit!=Integer.MAX_VALUE){
+        while(fit != Integer.MAX_VALUE){
+            // Check if the maximum allowed time has been exceeded
+            if (System.currentTimeMillis() - startTime > maxTimeMillis) {
+                System.out.println("Maximum time exceeded.");
+                break;
+            }
+    
             for (moveNode ls : pop){
                 winner = ls;
                 int newFit = fitness(ls, m1);
-                if(newFit>fit){
+                if(newFit > fit){
                     fit = newFit;
                 }
             }
+    
             ArrayList<moveNode> pop2 = new ArrayList<moveNode>();
-            for (int i=1; i<pop.size(); i++){
+            for (int i = 1; i < pop.size(); i++){
                 Random r = new Random();
-                moveNode parent1 = new moveNode();
-                moveNode parent2 = new moveNode();
-                parent1 = pop.get((r.nextInt(pop.size())));
-                parent2 = pop.get((r.nextInt(pop.size())));
-                moveNode child = new moveNode(parent1.getLoc(), reproduce(parent1.getPreMoves(), parent2.getPreMoves()));
+                moveNode parent1 = pop.get(r.nextInt(pop.size()));
+                moveNode parent2 = pop.get(r.nextInt(pop.size()));
+                moveNode child = new moveNode(m1.getStart(), reproduce(parent1.getPreMoves(), parent2.getPreMoves())); // Ensure parent location is always the start
                 Random r2 = new Random();
                 int r1 = r2.nextInt(10);
-                if(r1<2){
+                if (r1 < 9){
                     child = new moveNode(child.getLoc(), mutate(child.getPreMoves()));
                     pop2.add(child);
                 }
-                pop = pop2; 
-            }
+            } 
+            pop = pop2;
         }
         return winner;
-
-    } 
+    }
 }
 
